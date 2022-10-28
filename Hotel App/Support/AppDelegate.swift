86ff2f8679621eaa8bd1401d0036904f471f9b01
Ivan Puzanov <ivanpuzan0v.dev@gmail.com>
2022-10-28 15:25:37 +0300

    Default project initialisation

---
+++ Hotel App/Support/AppDelegate.swift
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.swift
+//  Hotel App
+//
+//  Created by Ivan Puzanov on 28.10.2022.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    lazy var window: UIWindow? = UIWindow(frame: UIScreen.main.bounds)
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+}
