62a3cb1b706d7ee59bb03952df1b540cfc76d801
Ivan Puzanov <ivanpuzan0v.dev@gmail.com>
2022-10-28 15:17:32 +0300

    Initial Commit

---
+++ Hotel App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Hotel App
+//
+//  Created by Ivan Puzanov on 28.10.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
